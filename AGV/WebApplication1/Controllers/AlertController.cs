7110b5c742da853e56e5c8f29c2cd9213397ec2f
Henderickx Silke (s097336) <silke.henderickx@student.ap.be>
2018-12-12 10:35:02 +0100

    xmlhandler init

--- AGV/WebApplication1/Controllers/AlertController.cs
+++ AGV/WebApplication1/Controllers/AlertController.cs
@@ -5,16 +5,31 @@ using System.Net;
 using System.Net.Http;
 using System.Threading.Tasks;
 using System.Web.Http;
+using System.Xml;
+using WebApplication1;
 
 namespace AGVWebAPI.Controller
 {
     public class AlertController : ApiController
     {
-        public async Task<string> Post()
+        public async void Post()
         {
-            string result = await Request.Content.ReadAsStringAsync();
-            
-            return result;
+            string alertXML = await Request.Content.ReadAsStringAsync();
+
+            // xml validation to xsd
+
+            // timestamp handler
+            XMLHandler xmlHandler = new XMLHandler();
+
+            //string timestamp = xmlHandler.getBetween(alertXML, "<TimeStamp>", "</TimeStamp>");
+            //string newTimestamp = timestamp.
+            // xml string to Model
+
+            alertXML = alertXML.Replace("-", "=");
+            Staging_Alert alert = xmlHandler.MapXmlToStagingAlert(alertXML);
+
+            // alert model AddAlert()
+
         }
     }
 }
---
+++ AGV/WebApplication1/XMLHandler.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Xml.Serialization;
+
+namespace WebApplication1
+{
+    public class XMLHandler
+    {
+        public Staging_Alert MapXmlToStagingAlert(string xmlString)
+        {
+            XmlSerializer serializer = new XmlSerializer(typeof(Staging_Alert), new XmlRootAttribute("AGVAlert"));
+            StringReader stringReader = new StringReader(xmlString);
+            Staging_Alert alert = (Staging_Alert) serializer.Deserialize(stringReader);
+            return alert;
+        }
+
+        public string getBetween(string strSource, string strStart, string strEnd)
+        {
+            int Start, End;
+            if (strSource.Contains(strStart) && strSource.Contains(strEnd))
+            {
+                Start = strSource.IndexOf(strStart, 0) + strStart.Length;
+                End = strSource.IndexOf(strEnd, Start);
+                return strSource.Substring(Start, End - Start);
+            }
+            else
+            {
+                return "";
+            }
+        }
+    }
+
+}
